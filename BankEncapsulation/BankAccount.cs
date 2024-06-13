c54e6a1198b8d911fed39b4b03062fc0b0d41749
Douglas <douglas-brumley@hotmail.com>
2024-06-13 15:46:35 -0400

    Excercise Completed

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+
+        public double _balance = 0;
+
+        public void Deposit(double money)
+        {
+            _balance += money;
+        }
+
+        public void GetBalance()
+        {
+            Console.WriteLine($"{_balance}");
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            BankAccount account1 = new BankAccount();
+            account1. Deposit(500);
+            account1. GetBalance();
         }
+
+
+
     }
+
+
 }
