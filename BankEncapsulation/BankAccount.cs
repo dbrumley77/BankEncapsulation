fcaa02ec7e9c4a924ef2177131b17f32f5a5932d
Douglas <douglas-brumley@hotmail.com>
2024-08-22 18:39:40 -0400

    Added Loop for Multiple Deposit Option

--- BankEncapsulation/BankAccount.cs
+++ BankEncapsulation/BankAccount.cs
@@ -19,35 +19,74 @@ namespace BankEncapsulation
             Console.WriteLine();
             _balance += money;
             Console.WriteLine($"Your checking account balance after deposit is ${_balance}");
-            Console.WriteLine();
+            
 
         }
 
 
-        public void AdditionalDeposits()
-        {
-            Console.WriteLine("Would you like to make another deposit: Yes or No");
-            string userAnswer = Console.ReadLine().ToLower();
+        //public void AdditionalDeposits()
+        //{
+        //    Console.WriteLine("Would you like to make another deposit: Yes or No");
+        //    string userAnswer = Console.ReadLine().ToLower();
 
 
-            if (userAnswer == "yes")
-            {
+        //    if (userAnswer == "yes")
+        //    {
 
-                Console.WriteLine("How much is the deposit amount?");
-                double depositAmount = double.Parse(Console.ReadLine());
-                _balance += depositAmount;
-                Console.WriteLine($"The new checking account balance is ${_balance}");
+        //        Console.WriteLine("How much is the deposit amount?");
+        //        double depositAmount = double.Parse(Console.ReadLine());
+        //        _balance += depositAmount;
+        //        Console.WriteLine($"The new checking account balance is ${_balance}");
                 
 
-            }
+        //    }
 
-            if (userAnswer == "no")
+        //    if (userAnswer == "no")
+        //    {
+        //        Console.WriteLine("Transaction completed: Have a good day");
+        //    }
+
+
+
+        //}
+
+        public void AdditionalDeposits()
+        {
+            while (true)
             {
-                Console.WriteLine("Transaction completed: Have a good day");
+                Console.WriteLine();
+                Console.WriteLine("What would you like to make another deposit?");
+                Console.WriteLine("Enter 1 for \"Yes\"");
+                Console.WriteLine("Enter 2 for \"No\"");
+
+                var parsedSucces = int.TryParse(Console.ReadLine(), out int option);
+
+                if (!parsedSucces)
+                {
+                    Console.WriteLine("Please enter a valid response.");
+                    Thread.Sleep(2000);
+                    Console.Clear();
+                    continue;
+                }
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine();
+                        Console.WriteLine("How much is the deposit amount?");
+                        Console.WriteLine();
+                        double depositAmount = double.Parse(Console.ReadLine());
+                        _balance += depositAmount;
+                        Console.WriteLine();
+                        Console.WriteLine($"The updated checking account balance is ${_balance}");
+                        break;
+                    case 2:
+                        Console.WriteLine();
+                        Console.WriteLine("Transaction completed: Have a good day");
+                        return;
+                }
+
             }
-
-
-
         }
 
 
